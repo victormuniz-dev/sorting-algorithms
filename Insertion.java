382f59e4b496bd71a793664b59ccaa4153a8fda5
victormuniz <dev.victormuniz@gmail.com>
2024-12-28 17:18:03 -0300

    insertion-sort

---
+++ Insertion.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+class Insertion{
+        public static void main(String[] args){
+                Scanner sc = new Scanner(System.in);
+                String input = sc.nextLine();
+
+                int[] arr = Arrays.stream(input.split(" ")).mapToInt(Integer::parseInt).toArray();
+                System.out.println(insertionSort(arr));
+        }
+
+        static int[] insertionSort(int[] v){
+                for (int i = 1; i < v.length; i++){
+                        int j = i;
+                        while (j > 0 && v[j] < v[j - 1]){
+                                int aux = v[j];
+                                v[j] = v[j - 1];
+                                v[j - 1] = aux;
+                                j--;
+                        }
+                }
+                return v;
+        }
+}
+
